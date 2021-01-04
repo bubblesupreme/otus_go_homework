cfa78b3d7607e53d7cfc4c61d564d28b5bbd7967
Svetlana Emelianova <lana-22@mail.ru>
2021-01-04 17:52:25 +0300

    LRU cache implementation

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -5,33 +5,50 @@ type Key string
 type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
-	Clear() // Очистить кэш
+	Clear()
 }
 
 type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
-	_ cacheItem
+	capacity int
+	queue    List
+	items    map[Key]*ListItem
 }
 
 type cacheItem struct {
-	// Place your code here
+	key   Key
+	value interface{}
 }
 
-func NewCache(_ int) Cache {
-	return &lruCache{}
+func NewCache(capacity int) Cache {
+	return &lruCache{capacity, NewList(), make(map[Key]*ListItem, capacity)}
 }
 
-func (c *lruCache) Set(_ Key, _ interface{}) bool {
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	newItem := cacheItem{key, value}
+	if v, ok := c.items[key]; ok {
+		c.queue.MoveToFront(v)
+		c.queue.Front().Value = newItem
+		return true
+	}
+	if c.capacity <= c.queue.Len() {
+		delete(c.items, c.queue.Back().Value.(cacheItem).key)
+		c.queue.Remove(c.queue.Back())
+	}
+	c.items[key] = c.queue.PushFront(newItem)
 	return false
 }
 
-func (c *lruCache) Get(_ Key) (interface{}, bool) {
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	if v, ok := c.items[key]; ok {
+		c.queue.MoveToFront(v)
+		return v.Value.(cacheItem).value, true
+	}
 	return nil, false
 }
 
 func (c *lruCache) Clear() {
-
+	for c.queue.Front() != nil {
+		c.queue.Remove(c.queue.Front())
+	}
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
